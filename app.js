b31026da3cd3bad63941c2bd1af89cbd406d3484
= <rc.info@protonmail.com>
2022-09-24 16:23:18 -0500

    added routes, controllers

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
+'use strict'
+
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path')
+require('dotenv').config()
+const authRouter = require('./routes/authRoutes')
 
 const app = express();
-
-// middleware
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 // view engine
 app.set('view engine', 'ejs');
 
-// database connection
-const dbURI = 'mongodb+srv://shaun:test1234@cluster0.del96.mongodb.net/node-auth';
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
-  .then((result) => app.listen(3000))
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {})
   .catch((err) => console.log(err));
 
 // routes
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.use(authRouter)
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server listening on port ${process.env.PORT}`)
+})
+
