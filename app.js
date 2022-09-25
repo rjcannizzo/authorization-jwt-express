2171f35409f9f1a3923bf158a3c243a4d9884f32
= <rc.info@protonmail.com>
2022-09-24 19:43:40 -0500

    added cookie-parser...ready to add jwt

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ const mongoose = require('mongoose');
 const path = require('path')
 require('dotenv').config()
 const authRouter = require('./routes/authRoutes')
+const cookieParser = require ('cookie-parser')
 
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+app.use(cookieParser())
 
 // view engine
 app.set('view engine', 'ejs');
@@ -22,6 +24,20 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRouter)
+
+app.get('/read', (req, res) => {
+  const cookies = req.cookies
+  console.log(cookies);
+  res.send('OK')
+})
+
+app.get('/set', (req, res) => {
+  // res.setHeader('Set-Cookie', 'newuser=true')
+  res.cookie('newuser', true, {maxAge: 1000 * 60 * 60 * 24, httpOnly: true})
+  res.cookie('employee', false, {maxAge: 1000 * 60 * 60 * 24, httpOnly: true})
+  res.send('cookie set...')
+
+})
 
 app.listen(process.env.PORT, () => {
   console.log(`Server listening on port ${process.env.PORT}`)
